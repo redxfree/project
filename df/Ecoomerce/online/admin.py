c59f97e4dcc88a8c18251d29d6bff296f0fb0d5b
redxfree <100827526+redxfree@users.noreply.github.com>
2022-11-08 10:43:53 +0530

    Delete df directory

--- df/Ecoomerce/online/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, Book, Product, Cart
-# Register your models here.
-
-admin.site.register(Category)
-admin.site.register(Book)
-admin.site.register(Product)
-admin.site.register(Cart)
